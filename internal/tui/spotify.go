45db6f3a84586abec6faab20c635e0110514c173
armando <armando@pop-os.localdomain>
2023-02-12 14:53:32 -0800

    Completed refactoring and doing this with elm architecture mindset

--- internal/tui/spotify.go
+++ internal/tui/spotify.go
@@ -1,113 +1,275 @@
 package tui
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"spotify-tui/internal/auth/pcke"
+	"spotify-tui/internal/models"
 
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/zmb3/spotify/v2"
 )
 
-type spotifyModel struct {
-	choice        selection
-	choices       []string
-	cursor        int
-	artistsLayout *tea.Model
-	width         int
-	height        int
-	spotifyClient *spotify.Client
+type searchPrompt struct {
+	searching bool
+	textInput textinput.Model
 }
 
-type selection int
+type displayArtists struct {
+	selectedArtist bool
+	artists        []*models.Artist
+	list           list.Model
+}
 
-var (
-	// MAINMENU represents our main menu
-	MAINMENU = selection(0)
-	// ARTISTS represents artists
-	ARTISTS = selection(1)
-	// PLAYLISTS represents playlists
-	PLAYLISTS = selection(2)
-	// SONGS represents songs
-	SONGS = selection(3)
-)
+type displaySongs struct {
+	renderSongs  bool
+	selectedSong bool
+	songs        []*models.Song
+	list         list.Model
+}
 
-// New returns a bubbletea Model
-func New() tea.Model {
-	// use this to login to spotify
+type composite struct {
+	searchPrompt
+	displayArtists
+	displaySongs
+	spotifyClient *spotify.Client
+	width         int
+	height        int
+}
+
+// NewComposite returns a new composite
+func NewComposite() tea.Model {
 	client, err := pcke.Auth()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	textInput := textinput.New()
+	textInput.Placeholder = "Enter Artists Name"
+	textInput.Focus()
+	textInput.CharLimit = 156
+	textInput.TextStyle.Height(10)
+	textInput.Width = 20
 
-	return &spotifyModel{
-		choices:       []string{"Artists", "Playlists", "Tracks"},
-		cursor:        0,
-		choice:        0,
+	artistList := list.New([]list.Item{}, list.DefaultDelegate{}, 0, 0)
+	artistList.Title = "Artists..."
+
+	songsList := list.New([]list.Item{}, list.DefaultDelegate{}, 0, 0)
+	songsList.Title = "Songs..."
+
+	return &composite{
 		spotifyClient: client,
+		searchPrompt: searchPrompt{
+			textInput: textInput,
+			searching: false,
+		},
+		displayArtists: displayArtists{
+			artists: nil,
+			list:    artistList,
+		},
+		displaySongs: displaySongs{
+			songs:        nil,
+			list:         songsList,
+			renderSongs:  false,
+			selectedSong: false,
+		},
 	}
+}
+
+// SpotifySearchArtistsMsg is a message sent
+type SpotifySearchArtistsMsg []*models.Artist
+
+// SelectedArtistMsg is message sent when we've selected an artist
+type SelectedArtistMsg *models.Artist
+
+// ArtistsResponse is the artists response we sent as a message
+type ArtistsResponse []*models.Artist
+
+// SongsResponse is the songs response we sent as a message
+type SongsResponse []*models.Song
+
+// SpotifySearchSongsRespMsg is a message signaling we got back an artists songs from spotify api
+type SpotifySearchSongsRespMsg []*models.Song
+
+// Init is the first function that will be called. It returns an optional
+// initial command. To not perform an initial command return nil.
+func (m searchPrompt) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+// Update is called when a message is received. Use it to inspect messages
+// and, in response, update the model and/or send a command.
+func (m searchPrompt) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+// View renders the program's UI, which is just a string. The view is
+// rendered after every Update.
+func (m searchPrompt) View() string {
+	return m.textInput.View()
 }
 
 // Init is the first function that will be called. It returns an optional
 // initial command. To not perform an initial command return nil.
-func (m *spotifyModel) Init() tea.Cmd {
+func (m *composite) Init() tea.Cmd {
 	return nil
 }
 
 // Update is called when a message is received. Use it to inspect messages
 // and, in response, update the model and/or send a command.
-func (m *spotifyModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var selectedModel tea.Model = m
+func (m *composite) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	var cmds []tea.Cmd
 	switch msg := msg.(type) {
-	// if it is a key press
-	case tea.WindowSizeMsg:
-		m.width, m.height = msg.Width, msg.Height
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-			// I need to load a sub-model, I think this is what I need to do
-		case "enter":
-			// because the mainmenu is ouor default menu, we default to this
-			m.choice = selection(m.cursor) + 1
-
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
+	// TODO: study bubble tea lists to see how they handle this stuff more cleanly
+	case SpotifySearchSongsRespMsg:
+		m.displaySongs.songs = SongsResponse(msg)
+		items := make([]list.Item, len(m.displaySongs.songs))
+		for i := range items {
+			items[i] = m.displaySongs.songs[i]
 		}
-		switch m.choice {
-		case MAINMENU:
-			selectedModel = m
-		case ARTISTS:
-			artistLayout := NewSearchArtistModel(m.spotifyClient, m.width, m.height)
-			selectedModel = artistLayout
-		case PLAYLISTS:
-			selectedModel = m
-		case SONGS:
-			selectedModel = m
+		m.displaySongs.list.SetItems(items)
+		m.displaySongs.renderSongs = true
+	case SpotifySearchArtistsMsg:
+		// handle the message being sent with the retrieved response sent from the command
+		m.displayArtists.artists = ArtistsResponse(msg)
+		items := make([]list.Item, len(m.displayArtists.artists))
+		for i := range items {
+			items[i] = m.displayArtists.artists[i]
+		}
+		m.displayArtists.list.SetItems(items)
+		m.searching = false
+	case tea.WindowSizeMsg:
+		// m.width, m.height = msg.Width, msg.Height
+		m.displayArtists.list.SetSize(msg.Width, msg.Height-10)
+		m.displaySongs.list.SetSize(msg.Width, msg.Height)
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC:
+			return m, tea.Quit
+		case tea.KeyEsc:
+			// the esc key is resolved for list.Model... ugggghh
+			m.searchPrompt.searching = false
+			m.searchPrompt.textInput.Reset()
+			m.searchPrompt.textInput.Focus()
+			for len(m.displayArtists.list.Items()) > 0 {
+				m.displayArtists.list.RemoveItem(0)
+			}
+			for len(m.displayArtists.artists) > 0 {
+				m.displayArtists.artists = m.displayArtists.artists[:len(m.displayArtists.artists)-1]
+			}
+		case tea.KeyEnter:
+			// TODO: handle when we are rendering the displaySongs model
+			if m.displaySongs.renderSongs {
+				m.handleSelectedSong()
+			} else if m.searchPrompt.textInput.Focused() {
+				m.handleSearching()
+			} else {
+				cmd := m.handleSelectedArtist()
+				cmds = append(cmds, cmd)
+			}
 		}
 	}
-	return selectedModel, nil
+
+	if m.searchPrompt.searching == true {
+		cmd := m.getArtists(m.searchPrompt.textInput.Value())
+		cmds = append(cmds, cmd)
+	}
+
+	if m.searchPrompt.textInput.Focused() {
+		m.searchPrompt.textInput, cmd = m.searchPrompt.textInput.Update(msg)
+		cmds = append(cmds, cmd)
+	} else if !m.searchPrompt.textInput.Focused() && !m.displaySongs.renderSongs {
+		// the esc key will be picked up
+		// I only want this to update when the searchPrompt isn't in focus
+		m.displayArtists.list, cmd = m.displayArtists.list.Update(msg)
+		cmds = append(cmds, cmd)
+	} else if m.displaySongs.renderSongs {
+		m.displaySongs.list, cmd = m.displaySongs.list.Update(msg)
+		cmds = append(cmds, cmd)
+	}
+
+	return m, tea.Batch(cmds...)
 }
 
 // View renders the program's UI, which is just a string. The view is
 // rendered after every Update.
-func (m *spotifyModel) View() string {
-	s := "Choices\n\n"
-	for i, choice := range m.choices {
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
-		s += fmt.Sprintf("%s %s\n", cursor, choice)
+func (m *composite) View() string {
+	if !m.displaySongs.renderSongs {
+		return m.searchPrompt.View() + "\n" +
+			m.displayArtists.list.View()
 	}
-	// The footer
-	s += "\nPress q to quit.\n"
-	return s
+	return m.displaySongs.list.View()
+}
+
+func (m *composite) getArtists(query string) tea.Cmd {
+
+	// return artists
+	return func() tea.Msg {
+		artists, _ := models.GetArtists(m.spotifyClient, query)
+		return SpotifySearchArtistsMsg(artists)
+	}
+}
+
+// func (m *composite) handleArtistsResponse()
+
+// Init is the first function that will be called. It returns an optional
+// initial command. To not perform an initial command return nil.
+func (m *displayArtists) Init() tea.Cmd {
+	return nil
+}
+
+// Update is called when a message is received. Use it to inspect messages
+// and, in response, update the model and/or send a command.
+func (m *displayArtists) Update(_ tea.Msg) (tea.Model, tea.Cmd) {
+	panic("not implemented") // TODO: Implement
+}
+
+// View renders the program's UI, which is just a string. The view is
+// rendered after every Update.
+func (m *displayArtists) View() string {
+	panic("not implemented") // TODO: Implement
+}
+
+func (m *composite) handleSelectedArtist() tea.Cmd {
+	// return songs
+	return func() tea.Msg {
+		// set this to true
+		m.displayArtists.selectedArtist = true
+
+		// get the selected item
+		seletectedItem := m.displayArtists.list.SelectedItem()
+		artist := seletectedItem.(*models.Artist)
+
+		// search the songs belonging to this artist
+		songs, _ := models.GetSongs(m.spotifyClient, artist.ID)
+		return SpotifySearchSongsRespMsg(songs)
+	}
+}
+
+func (m *composite) handleSearching() {
+	m.searchPrompt.searching = true
+	m.searchPrompt.textInput.Blur()
+}
+
+// TODO: handle error and send it as a message, or send the selected song as a message and handle it
+// I think that's better
+func (m *composite) handleSelectedSong() {
+
+	m.displaySongs.selectedSong = true
+	selectedItem := m.displaySongs.list.SelectedItem()
+	song := selectedItem.(*models.Song)
+
+	devices, _ := m.spotifyClient.PlayerDevices(context.Background())
+	m.spotifyClient.PlayOpt(context.Background(),
+		&spotify.PlayOptions{
+			URIs:     []spotify.URI{song.SongURI},
+			DeviceID: &devices[0].ID,
+		},
+	)
 }
